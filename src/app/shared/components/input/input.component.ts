0e4504628e49c3b393077b9dd6bf968790a54a9f
= <=>
2026-03-19 22:54:56 +0530

    [fix] dynamic input placeholder animation fix

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -26,10 +26,8 @@ export class InputComponent implements OnInit, OnDestroy {
   control!: AbstractControl;
   isPasswordVisible = false;
 
+  slideDown = false;
   currentKeywordIndex = 0;
-  currentKeyword = '';
-  nextKeyword = '';
-  isAnimating = false;
   intervalId: any;
 
   @Input({ required: true }) form!: FormGroup;
@@ -39,62 +37,29 @@ export class InputComponent implements OnInit, OnDestroy {
   @Output() onchange = new EventEmitter<any>();
   @Output() onblur = new EventEmitter<any>();
 
-  translateY = 0;
-
   ngOnInit(): void {
     this.control = this.form.get(this.controlName) as AbstractControl;
 
     if (this.config?.keywords?.length) {
-      this.currentKeyword = this.config.keywords[0];
-
       this.intervalId = setInterval(() => {
-        this.animateKeyword();
-      }, 2000);
+        this.animatePlaceholder();
+      }, 3000);
     }
   }
 
-  animateKeyword() {
-    if (!this.config?.keywords?.length) return;
-
-    // Step 1: current goes DOWN
-    this.translateY = 100;
+  animatePlaceholder() {
+    this.slideDown = true;
 
     setTimeout(() => {
       this.currentKeywordIndex =
-        (this.currentKeywordIndex + 1) % (this.config.keywords?.length ?? 1);
+        (this.currentKeywordIndex + 1) % (this.config.keywords?.length || 1);
 
-      this.currentKeyword =
-        this.config.keywords?.[this.currentKeywordIndex] || '';
-
-      // Step 2: place new word ABOVE (no animation)
-      this.translateY = -100;
-
-      requestAnimationFrame(() => {
-        // Step 3: slide into center
-        this.translateY = 0;
-      });
+      this.slideDown = false;
     }, 400);
   }
 
   ngOnDestroy(): void {
     if (this.intervalId) clearInterval(this.intervalId);
-  }
-
-  startAnimation() {
-    if (!this.config?.keywords?.length) return;
-
-    this.isAnimating = true;
-
-    const nextIndex =
-      (this.currentKeywordIndex + 1) % this.config.keywords.length;
-
-    this.nextKeyword = this.config.keywords[nextIndex];
-
-    setTimeout(() => {
-      this.currentKeyword = this.nextKeyword;
-      this.currentKeywordIndex = nextIndex;
-      this.isAnimating = false;
-    }, 400);
   }
 
   blockInvalidNumberKeys(event: KeyboardEvent) {
